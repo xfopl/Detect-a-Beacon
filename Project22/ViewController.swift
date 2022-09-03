649df9ecda7311d5b8c310825b011c9dccafd331
Aleksandr Veselov <fcaeoff@gmail.com>
2022-09-03 21:23:17 +0500

    Initial Commit

---
+++ Project22/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project22
+//
+//  Created by Александр Веселов on 9/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
